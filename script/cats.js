6d8d4b8331007379557a26949844846014d1038c
Natalya1110 <natalya.zatsepina83@yandex.ru>
2022-12-20 09:24:19 +0700

    Created Cats maket

---
+++ script/cats.js
@@ -0,0 +1,56 @@
+const cats = [
+    {
+        "name": "Персик",
+        "img_link": "https://img5.goodfon.ru/original/800x480/0/8a/kot-koteika-kote-ryzhii-serioznyi-mordochka-vzgliad-portret.jpg",
+        "age": 4,
+        "rate": 8,
+        "favourite": false,
+        "description": "Рыжий хитрец",
+        "id": 1111111
+    },
+    {
+        "name": "Нюша",
+        "img_link": "https://krasivosti.pro/uploads/posts/2021-07/1625907151_26-krasivosti-pro-p-koshki-s-zelenimi-glazami-poroda-koti-kras-28.jpg",
+        "age": 2,
+        "rate": 10,
+        "favourite": true,
+        "description": "Зеленоглазая красавица",
+        "id": 1111112
+    },
+    {
+        "name": "Снежок",
+        "img_link": "https://d17fnq9dkz9hgj.cloudfront.net/uploads/2018/04/Turkish-Angora-white-cat.jpg",
+        "age": 6,
+        "rate": 9,
+        "favourite": false,
+        "description": "Знает толк в отдыхе",
+        "id": 1111113
+    },
+    {
+        "name": "Филипп",
+        "img_link": "https://funart.pro/uploads/posts/2021-07/1627460549_12-funart-pro-p-vazhnii-kot-zhivotnie-krasivo-foto-14.jpg",
+        "age": 7,
+        "rate": 8,
+        "favourite": false,
+        "description": "Хозяин в доме",
+        "id": 1111114
+    },
+    {
+        "name": "Каспер",
+        "img_link": "https://s-zametki.ru/wp-content/uploads/2021/03/koshka-gryzet-provoda-kak-otuchit-zhivotnoe.jpg",
+        "age": 1,
+        "rate": 10,
+        "favourite": true,
+        "description": "Охотник и гроза мышей",
+        "id": 1111115
+    },
+    {
+        "name": "Кекс",
+        "img_link": "https://i.ibb.co/HC8kxvw/rejting-kormov-dlya-koshek-rossijskogo-proizvodstva.jpg",
+        "age": 2,
+        "rate": 9,
+        "favourite": false,
+        "description": "Любитель покушать",
+        "id": 1111116
+    }
+]
